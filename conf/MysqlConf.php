6508492b85275dc46d7831f60628ca8d4b28a02a
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2019-02-05 14:57:19 +0100

    update framework arbo

--- conf/MysqlConf.php
+++ conf/MysqlConf.php
@@ -2,7 +2,7 @@
 
 class mysqlConf extends Conf {
 	public function __construct() {
-		$cnf = file_get_contents(__DIR__.'/mysql.json');
+		$cnf = file_get_contents(__DIR__.'/../../external_confs/mysql.json');
 		$this->conf = json_decode($cnf, true);
 	}
 }
