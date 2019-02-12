96e2d85bd6f7cd5772c71bcd270d5d63afa5f212
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2019-02-12 14:49:09 +0100

    git dependencies autoloading

--- conf/DependenciesConf.php
+++ conf/DependenciesConf.php
@@ -4,6 +4,10 @@ namespace custom;
 class DependenciesConf extends \core\DependenciesConf {
 	public $conf = [
 		'queues_plugin' => 'https://github.com/nicolachoquet06250/mvc_framework_queues.git',
-		'oauth2_server' => 'https://github.com/bshaffer/oauth2-server-php.git',
+		'oauth2_server' => [
+			'repository' => 'https://github.com/bshaffer/oauth2-server-php.git',
+			'autoloader' => 'src/OAuth2/Autoloader.php',
+			'composer' => true,
+		],
 	];
 }
