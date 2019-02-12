bbf0478228249d83dc76b0de57fe84b3f6fa9e6e
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2019-02-12 14:31:28 +0100

    add git a dependency for OAuth2 Server

--- conf/DependenciesConf.php
+++ conf/DependenciesConf.php
@@ -3,6 +3,7 @@ namespace custom;
 
 class DependenciesConf extends \core\DependenciesConf {
 	public $conf = [
-		'queues_plugin' => 'https://github.com/nicolachoquet06250/mvc_framework_queues.git'
+		'queues_plugin' => 'https://github.com/nicolachoquet06250/mvc_framework_queues.git',
+		'oauth2_server' => 'https://github.com/bshaffer/oauth2-server-php.git',
 	];
 }
